74acc8638a14cbb3e1021d7b857756cb26f65304
Mapala1965 <mapalamegha@gmail.com>
2021-09-23 13:14:47 -0400

    second commit

--- src/Welcome/HelloWorld.java
+++ src/Welcome/HelloWorld.java
@@ -18,12 +18,22 @@ public class HelloWorld {
 			fg = zz;
 		}
 		
+		public HelloWorld(String zz) {
+			fg=zz;
+		}
+
+		
+		
+		
 public static void main(String[]args) {
 			
 			HelloWorld dd	=new HelloWorld(500);
 			System.out.println(dd.ab);
 			HelloWorld dd1	=new HelloWorld(500,"Name");
 			System.out.println(dd1.ab+"  "+dd1.fg);
+			HelloWorld dd2	=new HelloWorld("SurName");
+			System.out.println(dd2.fg);
+
 
 			
 		
